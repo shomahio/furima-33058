727a7b005572d8e63e139456e6fba95994ef9cd3
shomahio <potter021401@gmail.com>
2020-12-24 22:30:08 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,         presence: true
-         validates :last_name,        presence: true
-         validates :first_name,       presence: true
-         validates :last_name_kana,   presence: true
-         validates :first_name_kana,  presence: true
+         validates :nickname,         uniqueness: true
+         validates :last_name,        presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday,         presence: true
-         
-  
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 6 }
+
 end
