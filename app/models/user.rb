5a2708a7dfbb3b713b5e8686e31ce6f10781eb5b
shomahio <potter021401@gmail.com>
2020-12-24 19:53:01 +0900

    表示機能の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,         presence: true
-         validates :last_name,        presence: true,uniqueness: true
+         validates :last_name,        presence: true
          validates :first_name,       presence: true
          validates :last_name_kana,   presence: true
          validates :first_name_kana,  presence: true
