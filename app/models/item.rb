afc4eb076f2cb14cee85198b8d03bbe8d0ba8bdb
shomahio <potter021401@gmail.com>
2020-12-26 16:20:03 +0900

    画像テーブルのヒモづけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :shipping_place
+  has_one_attached :image
 
   validates :name, :explanation,  presence: true
   validates :category_id,   numericality: { other_than: 1 } 
