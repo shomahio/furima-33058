6a6d138fbc5d64b6e9870bda54f98bc7547033e8
shomahio <potter021401@gmail.com>
2020-12-29 22:07:49 +0900

    削除機能の失敗にた時の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,16 +27,19 @@ class ItemsController < ApplicationController
 
   
   def update
-      if @item.update(item_params)
-        redirect_to root_path
-      else
+    if @item.update(item_params)
+      redirect_to root_path
+    else
         render :edit
       end
   end
 
   def destroy
-    @item.destroy
-    redirect_to action: :index
+    if @item.destroy
+      redirect_to action: :index
+    else
+      render :show
+    end
   end
 
   private
