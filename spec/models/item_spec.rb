502d10e153cd995fc931a9e9b252ca677dac4eb8
shomahio <potter021401@gmail.com>
2020-12-29 15:10:17 +0900

    変更しました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,12 +67,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
-      it "価格が300以下では投稿できない" do
+      it "価格が299以下では投稿できない" do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it "価格が9999999以上では投稿できない" do
+      it "価格が10000000以上では投稿できない" do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
