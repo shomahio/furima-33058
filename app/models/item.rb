5ef0d9fd589061ebb57e665f546c6a5a0a91ba5b
shomahio <potter021401@gmail.com>
2020-12-31 17:43:28 +0900

    オーダーモデル、コントローラー作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   belongs_to :shipping_place
   has_one_attached :image
+  has_one  :order
 
   with_options presence: true do
     validates :image
